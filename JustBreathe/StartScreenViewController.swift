a16a533ae1956dbe1ade502455329e7430ef88c9
Tommi Saarikangas <tommi.saarikangas@gmail.com>
2021-01-29 16:41:57 +0200

    Change title font

--- JustBreathe/StartScreenViewController.swift
+++ JustBreathe/StartScreenViewController.swift
@@ -25,13 +25,13 @@ class StartScreenViewController: UIViewController {
         let title = UILabel()
         title.text = "4-7-8"
         title.textColor = .white
+        title.font = UIFont.systemFont(ofSize: 24, weight: .semibold)
         view.addSubview(title)
         title.snp.makeConstraints { make in
             make.top.equalTo(calmImg.snp.bottom).offset(40)
             make.centerX.equalToSuperview()
         }
     }
-    
 }
 
 extension UIViewController {
