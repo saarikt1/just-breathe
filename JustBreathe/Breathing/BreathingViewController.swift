557eb48eee3cac435a3c7be32bd82b4d44758f56
Tommi Saarikangas <tommi.saarikangas@gmail.com>
2021-02-24 11:50:24 +0200

    Add text labels to breathing animation

--- JustBreathe/Breathing/BreathingViewController.swift
+++ JustBreathe/Breathing/BreathingViewController.swift
@@ -9,19 +9,27 @@ import UIKit
 
 class BreathingViewController: UIViewController {
     let breathingView = BreathingView()
-    var firstCycle = true
-    var holdBetweenCycles = 0
+    var breathingLabel = UILabel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         setBackground()
         
+        breathingLabel.text = "Inhale"
+        breathingLabel.font = UIFont.systemFont(ofSize: 18, weight: .semibold)
+        breathingLabel.textColor = R.color.white80()
+
         view.addSubview(breathingView)
+        view.addSubview((breathingLabel))
         breathingView.snp.makeConstraints { (make) in
             make.edges.equalToSuperview()
         }
+        breathingLabel.snp.makeConstraints { (make) in
+            make.centerX.equalToSuperview()
+            make.bottom.equalTo(breathingView.breatheLogo.snp.top).offset(-100)
+        }
         
-        startAnimation(inhale: 4, firstHold: 2, exhale: 6, secondHold: 1, cycles: 3)
+        startAnimation(inhale: 4, firstHold: 2, exhale: 6, secondHold: 0, cycles: 3)
     }
     
     func startAnimation(inhale: Int, firstHold: Int, exhale: Int, secondHold: Int, cycles: Int) {
@@ -32,16 +40,31 @@ class BreathingViewController: UIViewController {
             return
         }
 
-        holdBetweenCycles = firstCycle ? 0 : secondHold
-
-        UIView.animate(withDuration: TimeInterval(inhale), delay: TimeInterval(holdBetweenCycles), options: .curveEaseInOut, animations: {
+        UIView.animate(withDuration: TimeInterval(inhale), delay: 0, options: .curveEaseInOut, animations: {
+            self.breathingLabel.text = "Inhale"
+            self.breathingLabel.alpha = 0.0
             self.breathingView.breatheLogo.transform = rotateAndScaleUp
         }) { _ in
-            UIView.animate(withDuration: TimeInterval(exhale), delay: TimeInterval(firstHold), options: .curveEaseInOut, animations: {
-                self.breathingView.breatheLogo.transform = .identity
+
+            UIView.animate(withDuration: TimeInterval(firstHold), delay: 0, options: .curveLinear, animations: {
+                self.breathingLabel.text = ""
+                self.breathingLabel.alpha = 1.00
             }) { _ in
-                self.firstCycle = false
-                self.startAnimation(inhale: inhale, firstHold: firstHold, exhale: exhale, secondHold: secondHold, cycles: cycles-1)
+                
+                UIView.animate(withDuration: TimeInterval(exhale), delay: 0, options: .curveEaseInOut, animations: {
+                    self.breathingLabel.text = "Exhale"
+                    self.breathingLabel.alpha = 0.0
+                    self.breathingView.breatheLogo.transform = .identity
+                }) { _ in
+                    
+                    UIView.animate(withDuration: TimeInterval(secondHold), delay: 0, options: .curveLinear, animations: {
+                        self.breathingLabel.text = ""
+                        self.breathingLabel.alpha = 1.00
+                    }) { _ in
+
+                        self.startAnimation(inhale: inhale, firstHold: firstHold, exhale: exhale, secondHold: secondHold, cycles: cycles-1)
+                    }
+                }
             }
         }
     }
