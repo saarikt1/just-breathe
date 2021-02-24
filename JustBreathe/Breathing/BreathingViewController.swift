96366d49757955e3da704a80a4a770ab44c3d875
Tommi Saarikangas <tommi.saarikangas@gmail.com>
2021-02-24 11:06:25 +0200

    Implement breathing animation cycles

--- JustBreathe/Breathing/BreathingViewController.swift
+++ JustBreathe/Breathing/BreathingViewController.swift
@@ -9,7 +9,9 @@ import UIKit
 
 class BreathingViewController: UIViewController {
     let breathingView = BreathingView()
-    
+    var firstCycle = true
+    var holdBetweenCycles = 0
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setBackground()
@@ -19,13 +21,28 @@ class BreathingViewController: UIViewController {
             make.edges.equalToSuperview()
         }
         
-        startAnimation(inhale: 4, firstHold: 2, exhale: 6, secondHold: 0)
+        startAnimation(inhale: 4, firstHold: 2, exhale: 6, secondHold: 1, cycles: 3)
     }
     
-    func startAnimation(inhale: Int, firstHold: Int, exhale: Int, secondHold: Int) {
-        UIView.animate(withDuration: TimeInterval(inhale), delay: 2, options: [.curveEaseInOut, .repeat, .autoreverse]) {
-            self.breathingView.breatheLogo.transform = CGAffineTransform(rotationAngle: CGFloat.pi)
-            self.breathingView.breatheLogo.transform = CGAffineTransform(scaleX: 1.5, y: 1.5)
+    func startAnimation(inhale: Int, firstHold: Int, exhale: Int, secondHold: Int, cycles: Int) {
+        let rotate = CGAffineTransform(rotationAngle: 3.0)
+        let rotateAndScaleUp = rotate.scaledBy(x: 1.5, y: 1.5)
+
+        if cycles < 1 {
+            return
+        }
+
+        holdBetweenCycles = firstCycle ? 0 : secondHold
+
+        UIView.animate(withDuration: TimeInterval(inhale), delay: TimeInterval(holdBetweenCycles), options: .curveEaseInOut, animations: {
+            self.breathingView.breatheLogo.transform = rotateAndScaleUp
+        }) { _ in
+            UIView.animate(withDuration: TimeInterval(exhale), delay: TimeInterval(firstHold), options: .curveEaseInOut, animations: {
+                self.breathingView.breatheLogo.transform = .identity
+            }) { _ in
+                self.firstCycle = false
+                self.startAnimation(inhale: inhale, firstHold: firstHold, exhale: exhale, secondHold: secondHold, cycles: cycles-1)
+            }
         }
     }
 }
